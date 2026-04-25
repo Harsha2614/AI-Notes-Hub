a784da568a43dd1536b1b52b955c11e87972cd5e
Harsha <harsha.rmb31@gmail.com>
2026-04-25 13:20:01 +0530

    issues rectified

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ai-notes-hub-seven.vercel.app/"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
