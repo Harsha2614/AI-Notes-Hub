54e774f2f31cbb0ce7fe78aa038e175eaac5b715
Harsha <harsha.rmb31@gmail.com>
2026-04-25 12:55:34 +0530

    commit in frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ai-notes-hub-qf3m.onrender.com/"
 });
 
 API.interceptors.request.use((req) => {
